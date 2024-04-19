a5afb1369deb40c15677d08aa32fc0811beb2819
abhishek-classcard <88773474+abhishek-classcard@users.noreply.github.com>
2024-04-19 13:32:21 +0530

    fix: update the functionality, remove logs

--- src/LottieAnimation.vue
+++ src/LottieAnimation.vue
@@ -110,7 +110,6 @@ export default {
       }, this.getRandomInt(this.loopDelayMin, this.loopDelayMax == 0 ? this.loopDelayMin : this.loopDelayMax));
     },
     handleExecutionComplete() {
-      console.log("handleExecutionComplete func");
       this.$emit("complete");
       this.anim.removeEventListener("complete");
     },
@@ -121,7 +120,6 @@ export default {
     },
     anim: {
       handler() {
-        console.log("in handler");
         if (this.anim != null) {
           this.anim.addEventListener("complete", () => {
             this.handleExecutionComplete();
